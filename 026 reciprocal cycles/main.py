e1e7c613b6b628c643a6dcafd36e183da9ed7dbb
Kyle <kchatman@gmail.com>
2022-02-11 16:57:35 -0800

    026 add result

--- 026 reciprocal cycles/main.py
+++ 026 reciprocal cycles/main.py
@@ -14,7 +14,7 @@
 # It can be seen that 1/7 has a 6-digit recurring cycle.
 # Find the value of d < 1000 for which 1/d contains the longest 
 # recurring cycle in its decimal fraction part.
-# Result: 
+# Result: 983
 
 import math
 from typing import Set, List, Tuple, Dict
