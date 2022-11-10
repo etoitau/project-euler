fafeb2f001987223274f55892b254fe152cbef2f
Kyle <kchatman@gmail.com>
2022-11-09 21:29:25 -0800

    084

--- 084 monopoly odds/main.py
+++ 084 monopoly odds/main.py
@@ -46,7 +46,7 @@
 # 102400.
 # If, instead of using two 6-sided dice, two 4-sided dice are used, find the 
 # six-digit modal string.
-# Result: 
+# Result: 101524
 
 from typing import Callable, Generator, Set, List, Tuple, Dict
 import time
@@ -59,7 +59,7 @@ import os
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-D = 6
+D = 4
 BOARD_SIZE = 40
 GO = 0
 JAIL = 10
@@ -88,59 +88,93 @@ def get_empty_board_graph() -> List[List[float]]:
     return [[0.0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
 def add_dice_moves(board: List[List[float]]):
-    jail_correction = pow(1 / D, 3)
-    move_chance = (1 - jail_correction) / D 
+    # From each space, what are the probabilities you will end up at each
+    # space from a normal dice roll
+
+    # If this is your third double roll, after a non-double, 
+    # go to jail instead.
+    three_doubles = pow(1 / D, 3)
+    # But if your roll four rolls ago was a double, then the last 
+    # one sent you to jail and you just got out. 
+    not_double = (1 - 1 / D)
+    # You could also be just out of jail due to a chance or 
+    # community chest card in one of the last two turns, 
+    # so we need some further correction.
+    # There are six ch and cc spaces and each has 1/16 go to jail cards.
+    # This assumes even probability for each space which obviously isn't 
+    # exactly true (it's what we're trying to figure out!) but maybe it's
+    # okay as a first order approximation going into the problem.
+    no_card_gtj = pow(1 - (6/40 * 1/16), 2)
+    # You could also be just out of jail from landing on the go to jail 
+    # space in the last two turns.
+    no_space_gtj = pow(1 - 1/40, 2)
+    jail_correction = three_doubles * not_double * no_card_gtj * no_space_gtj
+    move_chance = (1 - jail_correction) / (D * D) 
     for space in range(BOARD_SIZE):
-        for jump in range(D):
-            board[space][(space + jump + 1) % BOARD_SIZE] += move_chance
+        for roll1 in range(D):
+            for roll2 in range(D):
+                board[space][(space + roll1 + roll2 + 2) % BOARD_SIZE] += move_chance
         board[space][JAIL] += jail_correction
         
 def add_go_to_jail(board: List[List[float]]):
+    # Any probability assigned to the go to jail space should be relocated to jail
     for space in range(BOARD_SIZE):
         board[space][JAIL] += board[space][G2J]
         board[space][G2J] = 0.0
 
 def add_cc(board: List[List[float]]):
+    # Reallocate probability assigned to community chest spaces
     normal_move_chance = 14 / 16
     for space in range(BOARD_SIZE):
         for cc in CC:
             special_chance = board[space][cc] * (1 - normal_move_chance)
-            board[space][cc] *= normal_move_chance
+            board[space][cc] -= special_chance
             board[space][GO] += special_chance / 2
             board[space][JAIL] += special_chance / 2
 
 def add_chance(board: List[List[float]]):
+    # Reallocate the probability assigned to chance spaces
     normal_move_chance = 6 / 16
     for space in range(BOARD_SIZE):
         for ch in CH:
             special_chance = board[space][ch] * (1 - normal_move_chance)
-            board[space][ch] *= normal_move_chance
+            board[space][ch] -= special_chance
             board[space][GO] += special_chance / 10
             board[space][JAIL] += special_chance / 10
             board[space][11] += special_chance / 10  # C1
             board[space][24] += special_chance / 10  # E3
             board[space][39] += special_chance / 10  # H2
-            board[space][5] += special_chance / 10  # C1
-            board[space][next_railroad(space)] += special_chance / 5
-            board[space][next_utility(space)] += special_chance / 10
+            board[space][5] += special_chance / 10  # R1
+            board[space][next_railroad(ch)] += special_chance / 5
+            board[space][next_utility(ch)] += special_chance / 10
             board[space][(space - 3 + BOARD_SIZE) % BOARD_SIZE] += special_chance / 10
 
 def get_board_graph() -> List[List[float]]:
+    # From each space, what is the probability you will end up 
+    # at each other space after your next roll?
     board = get_empty_board_graph()
     add_dice_moves(board)
     add_go_to_jail(board)
+    # Note chance needs to go before community chest 
+    # because chance's back 3 can put you on cc
+    add_chance(board)  
     add_cc(board)
-    add_chance(board)
     return board
 
 def get_next_turn(board_state: List[float], graph: List[List[float]]) -> List[float]:
     next_state = get_empty_board()
+    # For each space in the start state, we distribute that space's 
+    # probability to all the other spaces according to the ratios 
+    # we previously determined.
     for start in range(BOARD_SIZE):
         to_distribute = board_state[start]
         for dest in range(BOARD_SIZE):
             next_state[dest] += to_distribute * graph[start][dest]
+    return next_state
 
 def diff_states(state1: List[float], state2: List[float]) -> float:
+    # Get a metric of how much the probability distribution has changed
+    # between two states.
     diff = 0
     for space in range(BOARD_SIZE):
         diff += abs(state1[space] - state2[space])
@@ -148,8 +182,10 @@ def diff_states(state1: List[float], state2: List[float]) -> float:
 
 def find_steady_state() -> List[float]:
     graph = get_board_graph()
-    start = get_empty_board()
-    start[0] = 1
+    # Start with probability equally distributed
+    start = [1/BOARD_SIZE for _ in range(BOARD_SIZE)]
+    # Use the graph to redistribute the probability 
+    # until we reach steady state.
     end = get_next_turn(start, graph)
     while diff_states(start, end) > 0.01:
         start = end
@@ -157,18 +193,21 @@ def find_steady_state() -> List[float]:
     return end
 
 def modal_string(board_state: List[float]):
+    # Make pairs of space and space probability
     pairs = [(i, board_state[i]) for i in range(len(board_state))]
-    heap = Heap(lambda a, b: b[1] - a[1], pairs)  # note reversed order to get a max heap
+    # Add to a max heap, so highest probability will be on top.
+    # note reversed order to get a max heap instead of min
+    heap = Heap(lambda a, b: b[1] - a[1], pairs)  
     top_3 = [heap.pop_root()[0] for _ in range(3)]
     ret = ""
     for space in top_3:
         if space < 10:
             ret += "0"
-        ret += space
+        ret += str(space)
     return ret
 
 if __name__ == '__main__':
     """ starts here """
     start = time.time()
-    print(modal_string(find_steady_state())) # 
-    print(time.time() - start) # 0. sec
+    print(modal_string(find_steady_state())) # 101524
+    print(time.time() - start) # 0.0070 sec
